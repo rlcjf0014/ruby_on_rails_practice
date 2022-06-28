787715b5efb02f134271ec922e6fef25bb6742c6
Joshua Song <joshuarule2010@gmail.com>
2022-06-27 23:04:04 -0500

    completed up to 7.3, tried out create of CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  # Rails provides a routes method named resources that maps all of the conventional 
+  # routes for a collection of resources, such as articles.
+  resources :articles
+
 end
